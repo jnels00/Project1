cfa41f86a2aec6d889a73378bf7af31b3fdea8df
Jacob Nelson <jnelson1@uon.edu.au>
2019-04-03 08:07:35 +0000

    added comment block

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/* Create string to encrypt/decrypt
+ encryption; e(x) = (m + k)(mod 26)where key = k, integer
+d(c) = (c - k)(mod 26) mod = % in C 
+%c tells printf and scanf to convert between ASCII and numbers
+c = c-65 convert letter to number i.e. A = 0, B = 1*/
+
+char str[100]
 
 int main()  {
-sam was here 19
-  return 0;
+    
+    
+    return 0;
 }
