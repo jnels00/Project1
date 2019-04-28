7e6fa2e29dceb19d3783b69ca3d75c001f8f0475
Jacob Nelson <jnelson1@uon.edu.au>
2019-04-28 07:41:15 +0000

    Progress

--- main.c
+++ main.c
@@ -5,20 +5,30 @@ d(c) = (c - k)(mod 26) mod = % in C
 %c tells printf and scanf to convert between ASCII and numbers
 c = c-65 convert letter to number i.e. A = 0, B = 1*/
 
-char str[100]
+void rotation (char str[], int key);
+
+int main()
+{
+char str[100];
 int key;
+
 printf("Enter a message:", ); // This message gets encrypted.
+scanf("%s", str);
 
 printf("Enter a Key:", ); // This is the adjustment factor for the roatation. 
 scanf("%d", &key); // Enters the 'key' (adjustment factor)
 
+return 0;
+}
+
+void rotation (char str[], int key);
+int i = 0;
+
+while 
 if (char >= 'a' && char <= 'z',){
-    char = char + key; //Adds key if letter entered is lower case. 
-}
-
-
-int main()  {
+    char = char + key; //Adds key if letter entered in lower case.
     
     
-    return 0;
 }
+    
+  
